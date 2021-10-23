f549c137dc35786dc6558823165ed3fd4994a2da
Joselo <impacevanend@gmail.com>
2021-10-22 20:59:47 -0500

    creando vista de aplicación

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class HelloApiVie(APIView):
+    
+    '''API View de prueba '''
+    
+    def get(self, request, format=None):
+        """Retornar lista de caracteristicas del APIview"""
+        an_apiview = [
+            'Usamos métodos HTTP como funciones (get, post, patch, put, delete)',
+            'ES similar a una vista tradicional de Django',
+            'Nos da el mayor control sobre la lógica de nuestra aplicación',
+            'Está mapeado manualmente a los urls',
+        ]
+
+        return Response({'message':'Hello','an_apiview':an_apiview})
+
